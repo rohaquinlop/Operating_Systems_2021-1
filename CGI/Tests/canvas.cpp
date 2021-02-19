dc8891a3776253cd26c15151dd75eb5551462dde
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-02-18 23:19:38 -0500

    HTML CGI Basic Template

---
+++ CGI/Tests/canvas.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  //HTML Basic Structure, created by Robin Quintero and Jhon Londoño
+
+  cout << "content-type: text/html" << endl << endl;
+  cout << "<html lang =\"en\">" << endl;
+
+  cout << "<head>" << endl;
+    cout << "<meta charset=\"UTF-8\">" << endl;
+    cout << "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">" << endl;
+    cout << "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" << endl;
+    cout << "<title>" << endl;
+      cout << "CGI CPP Canvas" << endl; //Change Title
+    cout << "</title>" << endl;
+  cout << "</head>" << endl;
+
+  cout << "<body>" << endl;
+  //HTML Body
+  
+  cout << "</body>" << endl;
+  cout << "</html>" << endl;
+
+  return 0;
+}
