a0963e17f2a120617647022402327f013c1c5533
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-02-20 22:20:03 -0500

    fst Assingment

---
+++ CGI/Assignments/CGI-SystemInformation.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  //HTML Basic Structure, created by Robin Quintero and Jhon Londoño
+
+  cout << "content-type: text/html" << endl << endl;
+  cout << "<html lang =\"en\">" << endl;
+
+  cout << "<head>" << endl;
+    cout << "<meta charset=\"UTF-8\">" << endl;
+    cout << "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">" << endl;
+    cout << "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" << endl;
+    cout << "<title>" << endl;
+      cout << "System Information" << endl; //Change Title
+    cout << "</title>" << endl;
+  cout << "</head>" << endl;
+
+  cout << "<body>" << endl;
+  //HTML Body
+  
+  //cout << "<>" << endl;
+  /*
+  - El numero de procesadores de una maquina
+  - El numero de procesos corriendo
+  - Cantidad de memoria de la maquina
+  - Total de interrupciones ejecutadas
+  */
+
+  cout << "<h1>System Information</h1>" << endl;
+  
+  cout << "</body>" << endl;
+  cout << "</html>" << endl;
+
+  return 0;
+}
