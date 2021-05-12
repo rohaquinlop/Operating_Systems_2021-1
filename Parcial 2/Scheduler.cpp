c878a7fa44f49aa7361b58b1eea341d505bb1c7f
rohaquinlop <rohaquinlop301@gmail.com>
2021-05-12 03:40:27 -0500

    Scheduler update

--- Parcial 2/Scheduler.cpp
+++ Parcial 2/Scheduler.cpp
@@ -1,2 +1,9 @@
 #include "Scheduler.h"
 
+template <class T>
+Scheduler<T>::Scheduler(){
+  this->actTime = 0;
+  this->jobQueue = queue<Process*>();
+  this->processedQueue = queue<Process*>();
+  this->readyQueue = priority_queue<Process*, vector<Process*>, T>();
+}
--- Parcial 2/Scheduler.h
+++ Parcial 2/Scheduler.h
@@ -1,5 +1,24 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
+#include <bits/stdc++.h>
+#include "Process.h"
+
+using namespace std;
+
+template <class T>
+class Scheduler{
+public:
+  //As processes enter the system, they are put into the job queue, which consists of all process in the system
+  int actTime;
+  queue<Process*> jobQueue;
+  queue<Process*> processedQueue;
+  priority_queue<Process*, vector<Process*>, T> readyQueue;
+
+  //Constructor
+  Scheduler();
+
+};
+
 
 #endif
