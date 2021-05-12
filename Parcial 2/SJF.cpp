dfaa60bcdc9c6021f954b03ebe5d75e4888e846b
rohaquinlop <rohaquinlop301@gmail.com>
2021-05-12 03:42:07 -0500

    SJF compare class

---
+++ Parcial 2/SJF.cpp
@@ -0,0 +1,8 @@
+#include "SJF.h"
+
+bool SJF::operator()(Process* a, Process* b){
+  if( a->getBurst() < b->getBurst() ){
+    return a->getArrival() < b->getArrival();
+  }
+  return a->getBurst() < b->getBurst();
+}
