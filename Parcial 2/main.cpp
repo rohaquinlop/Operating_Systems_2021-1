9295b6998ddeaab80a1db15be6656c54f8cb924e
rohaquinlop <rohaquinlop301@gmail.com>
2021-05-12 04:10:53 -0500

    Processes creation

--- Parcial 2/main.cpp
+++ Parcial 2/main.cpp
@@ -1,13 +1,43 @@
 #include <bits/stdc++.h>
 #include "Process.h"
+#include "Scheduler.h"
+#include "FCFS.h"
+#include "Priority.h"
+#include "SJF.h"
+#include "GanttChart.h"
+
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
+bool cmp(Process* a, Process* b){
+  return a->getArrival() < b->getArrival();
+}
 
 int main(){
-  Process p(1, 2, 3, 4, 5, 6);
+  int cntProcess, priority, burst, burstRange, arrival;
+  vector<Process*> processes;
+  srand(time(NULL)); // random seed
 
-  cout << p.getArrival() << endl;
+  cout << "Number of processes: ";
+  cin >> cntProcess;
+
+  cout << "Burst range: ";
+  cin >> burstRange;
+
+  for(int i = 0; i < cntProcess; i++){
+    priority = rand() % 10 + 1; // random (1..10)
+    arrival = rand()%(cntProcess+1); // random (0..cntProcess)
+    burst = rand() % burstRange + 1; // random (1.. burstRange)
+    processes.push_back(new Process(i, priority, arrival, burst));
+  }
+
+  sort(processes.begin(), processes.end(), cmp);
+
+  for(Process* p : processes){
+    cout << p->getPID() << " " << p->getArrival() << " " << p->getPriority() << " " << p->getBurst() << endl;
+  }
 
   return 0;
 }
