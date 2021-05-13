44b8150bd6e9fa50d781018d7616736dd6ffc422
rohaquinlop <rohaquinlop301@gmail.com>
2021-05-12 20:59:00 -0500

    Fixed Compare error

--- Parcial 2/SJF.cpp
+++ Parcial 2/SJF.cpp
@@ -1,7 +1,7 @@
 #include "SJF.h"
 
 bool SJF::operator()(Process* a, Process* b){
-  if( a->getBurst() < b->getBurst() ){
+  if( a->getBurst() == b->getBurst() ){
     return a->getArrival() < b->getArrival();
   }
   return a->getBurst() < b->getBurst();
